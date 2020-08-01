4f5ca0efedcf72597112c48f6c415d1a19f2f835
Nick <31454498+Meatwad440@users.noreply.github.com>
2020-07-31 22:22:39 -0400

    Create Figures.java
    updated methods

---
+++ Figures.java
@@ -0,0 +1,167 @@
+
+import java.util.Scanner;
+
+public class Figures {
+
+    private static Scanner scn = new Scanner (System.in);
+    
+    private static int size = 0;
+    
+    public static void main (String[]args) {
+	
+    	System.out.println ("\nSecond Program Assignment\nNick Hutson\nFigures Program\n");
+	
+    	//Request user input for size
+    	
+        System.out.print ("What size figure would you like? (odd numbers only): ");
+        
+        int n = scn.nextInt ();
+        
+        //Verification of odd number input and error case
+        
+        while (n % 2 == 0) {
+        	
+            System.out.println ("!!  Invalid figure size (odd numbers only please)  !!");
+            
+            System.out.println ("What size fugure would you like? (odd numbers only): ");
+            
+            n = scn.nextInt();
+            
+            System.out.println (" ");
+        }
+        
+        int size = n;
+        
+        printMenu();
+        
+        int option = scn.nextInt();
+        
+        //Flow control for input menu options
+        
+        while (true){
+        	
+            switch (option) {
+            
+                case 1:
+                	
+                	System.out.println (" ");
+                    printBox (n);
+                    System.out.println (" ");
+                    break;
+                    
+                case 2:
+                	
+                	System.out.println (" ");
+                    printDiamond (n);
+                    System.out.println (" ");
+                    break;
+                    
+                case 3:
+                	
+                	System.out.println (" ");
+                    printX (n);
+                    System.out.println (" ");
+                    break;
+                    
+                case 4:
+                	
+                	System.out.println (" ");
+                    System.out.println ("Well That Was Fun.");
+                    System.exit (0);
+            }
+
+            printMenu();
+            
+            option = scn.nextInt();
+        }
+        
+    }
+
+    //Input menu
+    
+    private static void printMenu () {
+        
+    	System.out.println("");
+    	
+    	System.out.println ("What Figure Would You Like To Print?");
+    	
+    	System.out.println("");
+        
+        System.out.println ("1) A Box");
+        
+        System.out.println ("2) A Diamond");
+        
+        System.out.println ("3) An X");
+        
+        System.out.println ("4) Exit Program");
+        
+        System.out.println("");
+        
+        System.out.print ("Select An Option And Press Enter: ");
+    }
+    
+    //Print Box Method
+    
+    private static void printBox (int size) {
+    	
+		for(int x = 0; x < size; x++) {
+
+		for(int y = 0; y < size; y++) 
+
+		System.out.print ("X");
+		
+		System.out.println ();
+		}
+        
+   }
+    
+    //Print Diamond Method
+    
+	public static void printDiamond(int size)
+	{
+		for(int i = 0; i < size; i++)
+		{
+			// Define variable for middle and inner space
+			int m = size / 2;
+			int k = Math.abs(m - i);
+			
+			for (int j = 0; j < k; j++)
+				System.out.print(" ");
+			System.out.print("X");
+			
+			for (int j = 0; j < (m - k) * 2 ; j++)
+				System.out.print(" ");
+			System.out.println("X");
+		}
+	}
+    
+    //Print X Method
+    
+	public static void printX(int size)
+	{
+		for(int i = 1 ; i <= size; i++)
+		{
+			// Define midpoint
+			int m = (size / 2) + 1;
+			// Define inner spaces
+			int k = Math.abs(m - i);
+			
+			for (int j = 0; j < (m - k -1); j++)
+				System.out.print(" ");
+			System.out.print("X");
+			
+			//skip midpoint
+			if(i == m)
+				System.out.println("");
+			
+			for (int j = 0; j < (2 * k) - 1 ; j++)
+				System.out.print(" ");
+			if(i != m)
+				System.out.println("X");
+		}
+	}
+
+    public static int getSize() {
+        return size;
+    }
+}
